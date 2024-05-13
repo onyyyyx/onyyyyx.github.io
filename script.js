fdf884840b3ec0662ebd032328017d561fc2f5dc
Onyx <shadowangel404@proton.me>
2024-05-13 19:47:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-      x.className += " responsive";
+        x.className += " responsive";
     } else {
-      x.className = "topnav";
+        x.className = "topnav";
     }
-  }
+}
+
+const fs = require('fs');
+
+function listFiles(dir, callback) {
+    fs.readdir(dir, function(err, files) {
+        if (err) return callback(err);
+        callback(null, files);
+    });
+}
+
+listFiles('images/', function(err, files) {
+    if (err) throw err;
+    files.forEach(function(file) {
+        const img = document.createElement('img');
+        const gallery = document.querySelector('.gallery')
+        
+        img.src = 'images/'+file;
+        
+        gallery.appendChild(img);
+        console.log(file);
+    });
+});
