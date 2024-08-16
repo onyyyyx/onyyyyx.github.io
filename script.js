f4815d6623b1a831a5e2d012cf6589e2fab16e13
onyyyyx <shadwangel404@proton.me>
2024-08-16 15:34:55 +0200

    Update

--- script.js
+++ script.js
@@ -6,25 +6,3 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
-const fs = require('fs');
-
-function listFiles(dir, callback) {
-    fs.readdir(dir, function(err, files) {
-        if (err) return callback(err);
-        callback(null, files);
-    });
-}
-
-listFiles('images/', function(err, files) {
-    if (err) throw err;
-    files.forEach(function(file) {
-        const img = document.createElement('img');
-        const gallery = document.querySelector('.gallery')
-        
-        img.src = 'images/'+file;
-        
-        gallery.appendChild(img);
-        console.log(file);
-    });
-});
